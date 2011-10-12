81708ee09f2fa445cc1a615560f8b14bc9c20356
Russell Toris <rctoris@wpi.edu>
2011-10-12 14:33:39 -0400

    aim camera function added

--- apps/nao_trick_or_treat/src/trick_or_treater.cpp
+++ apps/nao_trick_or_treat/src/trick_or_treater.cpp
@@ -43,7 +43,7 @@ trick_or_treater::trick_or_treater()
 
   // turn on the motors we need
   motion->set_stiffnesses("Body", 0.75);
-  tts->set_volume(0.75);
+  tts->set_volume(1);
 
   // get up in the correct position
   motion->sit();
@@ -76,33 +76,45 @@ trick_or_treater::~trick_or_treater()
 
 void trick_or_treater::joy_cback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  // set the walk speed
-  float theta = joy->axes.at(2) / (M_PI / 2.0);
-  motion->set_walk_velocity(joy->axes.at(1), joy->axes.at(0), theta, 1);
-
   vector<float> head_yaw_pitch;
-  if (theta == 0)
+
+  // check if this is an "aim camera" action
+  if (joy->buttons.at(5) == 1)
   {
-    // get the head positions for the walk
+    // get the head positions
     head_yaw_pitch.push_back(joy->axes.at(2));
     head_yaw_pitch.push_back(joy->axes.at(3));
   }
   else
   {
-    // get the head positions from the D-pad
-    head_yaw_pitch.push_back(joy->axes.at(4));
-    head_yaw_pitch.push_back(joy->axes.at(5));
+    // set the walk speed
+    float t = joy->axes.at(2) / (M_PI / 2.0);
+    motion->set_walk_velocity(joy->axes.at(1), joy->axes.at(0), t, (abs(joy->axes.at(0)) + abs(joy->axes.at(1))) / 2.0);
+    // check the head angles
+    if (t != 0)
+    {
+      // get the head positions for the walk
+      head_yaw_pitch.push_back(joy->axes.at(2));
+      head_yaw_pitch.push_back(joy->axes.at(3));
+    }
+    else
+    {
+      // get the head positions from the D-pad
+      head_yaw_pitch.push_back(joy->axes.at(4));
+      head_yaw_pitch.push_back(joy->axes.at(5));
+    }
+
+    // check for any TTS buttons
+    if (joy->buttons.at(0) == 1)
+      tts->say("Trick-or-Treat!");
+    else if (joy->buttons.at(1) == 1)
+      tts->say("Happy Halloween!");
+    else if (joy->buttons.at(2) == 1)
+      tts->say("Hey everybody, check out how adorable I am!");
   }
+
   //set the head position
   motion->set_angles(head, head_yaw_pitch, 0.4);
-
-  // check for any TTS buttons
-  if (joy->buttons.at(0) == 1)
-    tts->say("Trick-or-Treat!");
-  else if (joy->buttons.at(1) == 1)
-    tts->say("Happy Halloween!");
-  else if (joy->buttons.at(2) == 1)
-      tts->say("Hey everybody, check out how adorable I am!");
 }
 
 int main(int argc, char **argv)
