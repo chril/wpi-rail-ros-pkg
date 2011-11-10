888a4a84b6c8ea77c41a5a13eff40479e7368a5a
Russell Toris <rctoris@wpi.edu>
2011-11-10 12:21:29 -0500

    nao_speech node

---
+++ nao_rail/nao_speech/src/nao_speech/nao_speech.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# setup ROS
+import roslib;
+roslib.load_manifest('nao_vision')
+import rospy
+from std_msgs.msg import String
+import sys
+
+ # the path parameter is optional
+path = rospy.get_param("/naoqi/path", "")
+sys.path.append(path)
+
+# attempt to load NAOqi
+try:
+    from naoqi import ALProxy
+except ImportError:
+    rospy.logerr("NAOqi not found - Please check to see if it is in your PYTHONPATH variable.")
+    exit(1)
+    
+class NaoSpeech():
+    def __init__(self):
+        rospy.init_node("nao_speech")
+        
+        # attempt to get the host and port
+        try:
+            host = rospy.get_param("/naoqi/host")
+        except KeyError:
+            rospy.logerr("Unable to find parameter /naoqi/host")
+            exit(1)
+        try:
+            port = rospy.get_param("/naoqi/port")
+        except KeyError:
+            rospy.logerr("Unable to find parameter /naoqi/port")
+            exit(1)
+        
+        #connect to the Nao
+        try:
+            self.tts = ALProxy("ALTextToSpeech", host, port)
+        except Exception:
+            rospy.logerr("Unable to create speech proxy.")
+            exit(1)
+            
+        # get any optional parameters
+        volume = rospy.get_param("~volume", 0.5)
+        lang = rospy.get_param("~language", "English")
+        
+        #setup the node and the TTS module
+        self.tts.setLanguage(lang)
+        self.tts.setVolume(volume)
+        rospy.Subscriber("nao_say", String, nao_say_callback)
+
+        rospy.loginfo("Nao Speech Node Initialized")
+        
+    def nao_say_callback(self, data):
+        self.tts.post.say(data.data)
+
+if __name__ == '__main__':
+    NaoSpeech = NaoSpeech()
+    rospy.spin()
+
